72bb91d024a8d1255ff2614bca6b0e1bfa9179ef
Mony <thomasgutierrez720@gmail.com>
2026-07-14 22:00:19 -0500

    Initial project setup

---
+++ src/api/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return jsonify({
+		"status": "online",
+		"server": "My API",
+	})
+
+@app.route("/hello")
+def hello():
+	return jsonify({
+		"message": "Hello from Flask!"
+	})
+
+@app.route("/welcome")
+def welcome():
+	return "Cdawg wuz x"
+
+if __name__ == "__main__":
+	app.run(
+		host="0.0.0.0",
+		port=5000,
+		debug=True
+	)
