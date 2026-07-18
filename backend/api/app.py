6ab9f9e3c09f588266007b334ad95952d5a61cfd
Mony <Thomasgutierrez720@gmail.com>
2026-07-18 04:37:55 -0500

    API change: Adjusted API file structure

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,30 +1,9 @@
-from flask import Flask, jsonify
-from db import get_db_connection
+from flask import Flask
+from routes import register_routes
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-	return jsonify({
-		"status": "online",
-		"server": "My Flask API",
-	})
-
-@app.route("/user-test/<int:id>")
-def get_user(id):
-	db = get_db_connection()
-	cursor = db.cursor(dictionary=True)
-	cursor.execute(
-		"SELECT * FROM users WHERE id=%s",
-		(id,)
-	)
-	user = cursor.fetchone()
-	cursor.close()
-	db.close()
-
-	if user is None:
-		return jsonify({"error": "User not found"}), 404
-	return jsonify(user)
+register_routes(app)
 
 if __name__ == '__main__':
 	app.run(
