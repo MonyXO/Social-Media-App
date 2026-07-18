e925afc3d92afc79aea72155d3cb58c5edb77694
Mony <Thomasgutierrez720@gmail.com>
2026-07-18 05:06:31 -0500

    API change: Adjusted API

--- backend/api/db.py
+++ backend/api/db.py
@@ -4,6 +4,6 @@ def get_db_connection():
     return mysql.connector.connect(
 		host="localhost",
 		user="admin_mony",
-		password="$Mony5040$",
+		password="$Socialpassword01",
 		database="social_media_app"
 	)
