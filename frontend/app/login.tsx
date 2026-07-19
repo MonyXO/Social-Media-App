e2740cd0f8e98be0e623a833514c66d49b326b97
Mony <Thomasgutierrez720@gmail.com>
2026-07-19 06:42:44 -0500

    Fixed Expo and changed file structure of frontend

---
+++ frontend/app/login.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function LoginScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'Center'}}>
+            <Text style={{ fontSize: 24, marginBottom: 16}}>Login Screen</Text>
+        </View>
+    );
+}
