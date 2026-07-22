d9f0d085aa0ffe325638aaade1190e501b61390d
Mony <Thomasgutierrez720@gmail.com>
2026-07-22 01:23:32 -0500

    Fixed utils.py

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,6 +1,5 @@
 import random, string
 
-class utils:
-    def generate_token(length):
-        letters_and_digits = string.ascii_letters + string.letters_and_digits
+def generate_token(length):
+        letters_and_digits = string.ascii_letters + string.digits
         return ''.join((random.choice(letters_and_digits) for i in range(length)))
