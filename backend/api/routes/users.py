3909b052ef5de2aa1781c731a164d6118a1a9e52
Mony <Thomasgutierrez720@gmail.com>
2026-07-22 01:15:23 -0500

    Fixed utils.py

--- backend/api/routes/users.py
+++ backend/api/routes/users.py
@@ -13,7 +13,7 @@ def register_routes(app):
 			"server": "My Flask API",
 			"token": token,
 		})
-
+"""
 	@app.route("/login")
 	def login():
 		data = response.get_json()
@@ -38,7 +38,7 @@ def register_routes(app):
 			app_db.commit()
 			print('commited')
 			return make_response(jsonify( { 'Token': token } ), 200)
-
+"""
 
 
     @app.route("/user-test/<int:id>")
